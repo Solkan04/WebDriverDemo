52c9a25933fae261d5e55732978504660264c04a
Solkan04 <solkanmahesh@gmail.com>
2020-07-12 13:34:48 +0530

    Modified VerifyDropDownSortedOrNot Class

--- src/test/java/dropDowns/VerifyDropDownSortedOrNot.java
+++ src/test/java/dropDowns/VerifyDropDownSortedOrNot.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +14,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class VerifyDropDownSortedOrNot 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException
 	{
 		String path=System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", path+"\\Drivers\\chromedriver.exe");
@@ -24,9 +25,17 @@ public class VerifyDropDownSortedOrNot
 		
 		
 		WebElement selweb=driver.findElement(By.id("animals"));
+		
+		//scrolling to the element
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].scrollIntoView()", selweb);
+		Thread.sleep(10000);
+
+		//Getting all the options in dropdown
 		Select sel=new Select(selweb);
 		List<WebElement> options=sel.getOptions();
 		
+		//created two variables & storing all the dropdown values in it.
 		List originalList=new ArrayList();
 		List sortedTempList=new ArrayList();
 		for(WebElement e:options)
@@ -36,9 +45,12 @@ public class VerifyDropDownSortedOrNot
 		}
 		System.out.println("Original List: " +originalList);
 		System.out.println("Before Sorting Temp List: " +sortedTempList);
+		
+		//sorting onlt templist & not the originalList as we need to compare the two Lists
 		Collections.sort(sortedTempList);
 		System.out.println("After Sorting Temp List: " +sortedTempList);
 		
+		//comparing the two list variables
 		if(originalList == sortedTempList)
 			System.out.println("DropDown is Sorted");
 		else
